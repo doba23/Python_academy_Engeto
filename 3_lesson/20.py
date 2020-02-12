7168e1afd13d86ea873bdd52e27b71bc76163ab0
Dominik Bauchner <dbauchner@gmail.com>
2020-02-12 18:42:58 +0100

    finished homework - easy

---
+++ 3_lesson/20.py
@@ -0,0 +1,9 @@
+string01 = 'Bratislava'
+string02 = 'Budapest'
+
+string01 = set(string01)
+string02 = set(string02)
+# first task
+print (string01.intersection(string02))
+#second task
+print (string01.difference(string02))
