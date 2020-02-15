7368eeb68515813471854a6aba8413e3e631b379
Dominik Bauchner <dbauchner@gmail.com>
2020-02-15 15:13:26 +0100

    homework

---
+++ 4_lesson/27.py
@@ -0,0 +1,20 @@
+words = ['Python', 'is', 'a', 'widely', 'used',
+         'high-level', 'programming', 'language',
+         'for', 'general-purpose', 'programming,',
+         'created', 'by', 'Guido', 'van', 'Rossum',
+         'and', 'first', 'released', 'in', '1991.']
+words_2 = words.copy()
+words_lenght = []
+while words:
+    words_lenght.append(len(words[0]))
+    words = words [1:]
+
+
+print (words_with_lenght)
+print (words_2)
+
+words_with_lenght = {}
+while words
+
+
+print (words_with_lenght, words_2)
