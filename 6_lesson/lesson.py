f1cd0ae08dab0604a6b27c7094dcefed18a7c3c8
dell <dbauchner@gmail.com>
2020-02-17 18:21:13 +0100

    hello

--- 6_lesson/lesson.py
+++ 6_lesson/lesson.py
@@ -1,1 +1,10 @@
 #hello
+list1= ['banany', 'hrusky']
+for fruit in list1:
+    print (fruit)
+
+names = ['Jacob', 'Jana', 'Petr', 'Klara']
+
+for name in names:
+    if name.startswith('J'):
+        print (name)
