3b3aff74800f3af883f44489a66259cd84c6c89a
dell <dbauchner@gmail.com>
2020-02-17 18:56:07 +0100

    hello

--- 6_lesson/lesson.py
+++ 6_lesson/lesson.py
@@ -8,3 +8,9 @@ names = ['Jacob', 'Jana', 'Petr', 'Klara']
 for name in names:
     if name.startswith('J'):
         print (name)
+
+shop_cart = ['12', '1141', '12']
+
+for index, value in shop_cart:
+    print (index)
+    print (value)
