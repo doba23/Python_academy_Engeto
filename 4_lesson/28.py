990af03de0e443e67949d631c1d02b7240f28b3a
Dominik Bauchner <dbauchner@gmail.com>
2020-02-20 17:02:19 +0100

    homework

---
+++ 4_lesson/28.py
@@ -0,0 +1,16 @@
+number = int (input('Please enter your number:'))
+numbers = []
+
+#make list with all values
+index = 0
+while len(numbers) < number:
+    numbers.insert(index,((index+1)**(index+1)))
+    index += 1
+#print(numbers)
+
+# sum each value in list
+sum = 0
+while numbers:
+    one_number = numbers.pop()
+    sum = sum + one_number
+print('The result is:', sum)
