1a32c2f835a20396abfb276730d8eee16102b2fc
dell <dbauchner@gmail.com>
2020-02-24 18:23:18 +0100

    uloha ve vyucovani

--- 6_lesson/enumerate.py
+++ 6_lesson/enumerate.py
@@ -2,3 +2,11 @@ string1= 'aoidsf'
 
 for i in enumerate (string1):
     print (i)
+
+string1= 'aoidsf'
+index = 0
+
+ref_some_string = string1
+for i, letter in enumerate (string1):
+    letter = letter.upper()
+    print (letter, end=' ')
---
+++ 7_ lesson/vlastni_funkce.py
@@ -0,0 +1,6 @@
+
+def sum_num (a, b):
+    c = a + b
+    return c
+
+print (sum_num(1,2))
