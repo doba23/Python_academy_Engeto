d03cbc19a0d5cf01daf637e596529914750a2298
dell <dbauchner@gmail.com>
2020-02-24 19:07:01 +0100

    uloha ve vyucovani

--- 7_ lesson/vlastni_funkce_3.py
+++ 7_ lesson/vlastni_funkce_3.py
@@ -1,9 +1,11 @@
+#celkove secteni delky kazdeho retezce
+
+names  =  ['Ahoj','jak','se', 'mas']
+
 def sum (names : list):
     total = 0
     for name in names:
         total += len(name)
     print (total)
-
-names  =  ['Ahoj','jak','se', 'mas']
 
 sum(names)
