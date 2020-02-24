df5bf84425f63a44c342cbf1dd0cd5cb61835a08
Dominik Bauchner <dbauchner@gmail.com>
2020-02-24 16:30:44 +0100

    homework

--- 6_lesson/33.py
+++ 6_lesson/33.py
@@ -1,11 +1,11 @@
 listy = [6,4,3,5]
 print (listy)
-for l in range (len(listy)-2):
+for l in range (len(listy)-1):
     print ('outer')
-    for i in range (len(listy)-2):
+    for i in range (len(listy)-1):
         if listy [i] < listy [i+1]:
             print (i, 'IF')
-            next()
+            continue
         else:
             new = listy [i]
             listy[i]= listy [i+1]
