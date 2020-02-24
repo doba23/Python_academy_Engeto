b04859b74c1b9c13e82550fb30f33d31f8ddbcf7
dell <dbauchner@gmail.com>
2020-02-24 18:56:55 +0100

    uloha ve vyucovani

--- 7_ lesson/vlastni_funkce_2.py
+++ 7_ lesson/vlastni_funkce_2.py
@@ -3,3 +3,11 @@ def multi (symbol: str, count: int):
     return result
 
 print (multi ('*', 20))
+
+def multi_2 (symbol: str, count=10):
+    result = (symbol * count)
+    return result
+
+print (multi_2 ('se'))
+
+#pokud mam zadanou defaultni honotu paramteru numusim ji pri volani zadavat
