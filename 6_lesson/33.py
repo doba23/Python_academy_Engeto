872d1b385e51fdad7c969d14331526d3bb7a412f
Dominik Bauchner <dbauchner@gmail.com>
2020-02-25 19:19:27 +0100

    homework

--- 6_lesson/33.py
+++ 6_lesson/33.py
@@ -1,15 +1,19 @@
-listy = [6,4,3,5]
-print (listy)
-for l in range (len(listy)-1):
-    print ('outer')
-    for i in range (len(listy)-1):
-        if listy [i] < listy [i+1]:
-            print (i, 'IF')
+names = ['Michal', 'Pepa', 'Honza',
+        'Pavel', 'Lukas', 'Matej',
+        'Iva', 'Klara', 'Jana',
+        'Honza', 'Vasek','Milan', 'Michal'
+        ]
+
+for l in range (len(names)-1):
+    # print ('outer')
+    for i in range (len(names)-1):
+        if names [i] < names [i+1]:
+            # print (i, 'IF')
             continue
         else:
-            new = listy [i]
-            listy[i]= listy [i+1]
-            listy[i+1] = new
-            print (i, 'else')
-            print(listy)
-print (listy)
+            new = names [i]
+            names[i]= names [i+1]
+            names[i+1] = new
+            # print (i, 'else')
+            # print(names)
+print (names)
