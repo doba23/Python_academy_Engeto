65c204f7a98288129c68523eed7e32a6b043ed4b
Dominik Bauchner <dbauchner@gmail.com>
2020-02-27 21:43:23 +0100

    homework try

--- 6_lesson/36.py
+++ 6_lesson/36.py
@@ -1,2 +1,14 @@
 lenght_of_desk = int (input('Type lenght of desk:'))
 
+# create outer range based on input
+
+for_1 = (lenght_of_desk // 2)
+print(for_1)
+if ((lenght_of_desk) % 2) == 1:
+    for_1 += 1
+
+print (for_1)
+# inner range
+
+
+# for i in range(len(lenght_of_desk))
