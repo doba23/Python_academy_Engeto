b97d3788e8cea83f6b87154e45ec4fd7e4a2517e
Dominik Bauchner <dbauchner@gmail.com>
2020-02-27 21:49:35 +0100

    homework try

--- 6_lesson/36.py
+++ 6_lesson/36.py
@@ -4,9 +4,14 @@ lenght_of_desk = int (input('Type lenght of desk:'))
 
 for_outer = (lenght_of_desk // 2)
 for_inner = for_outer
-if (lenght_of_desk % 2) == 1:
-    for_outer += 1
+for_outer += 1 if (lenght_of_desk % 2) == 1
+
 
 print (for_outer)
 print (for_inner)
 # for i in range(len(lenght_of_desk))
+for i in range(6):
+
+    for j in range(for_inner):
+        print('* ', end="")
+    print('')
