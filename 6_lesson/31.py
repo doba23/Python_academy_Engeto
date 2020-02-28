a8ac8a11f3f5ee542fe35e7435b4b70e14b5a92a
Dominik Bauchner <dbauchner@gmail.com>
2020-02-28 08:59:20 +0100

    homework try

---
+++ 6_lesson/31.py
@@ -0,0 +1,6 @@
+sentence = 'A speech sound that is produced by comparatively open configuration of the vocal tract.'
+
+for i in sentence:
+    if sentence.find('a' or 'e' or 'i' or 'o' or 'u' or 'y'):
+        count_
+
