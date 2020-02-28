36c073a06a3e24c6589465330bee1fd127496b3b
Dominik Bauchner <dbauchner@gmail.com>
2020-02-28 13:03:03 +0100

    homework try

--- 6_lesson/31.py
+++ 6_lesson/31.py
@@ -1,8 +1,18 @@
 sentence = 'A speech sound that is produced by comparatively open configuration of the vocal tract.'
+sentence = sentence.replace(' ','')
+sentence = sentence.lower()
+print (sentence)
 
+vowel_count = 0
+cons_count = 0
 for i in sentence:
-    if sentence.find('a' or 'e' or 'i' or 'o' or 'u' or 'y'):
-        count_vowel +=1
+    print (i)
+    if i == ('a' or 'e' or 'i' or 'o' or 'u' or 'y'):
+        vowel_count += 1
+        print ('if')
+        print ('')
     else:
-        count_cons +=1
-
+        cons_count += 1
+        print('else')
+        print('')
+print('No. consonants:', cons_count, '| No. vowels:', vowel_count)
