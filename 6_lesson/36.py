939abb0a9e2270d5c3722980d1866fa400ec09f1
Dominik Bauchner <dbauchner@gmail.com>
2020-02-28 21:14:54 +0100

    homework try

--- 6_lesson/36.py
+++ 6_lesson/36.py
@@ -1,4 +1,5 @@
 lenght_of_desk = int (input('Type lenght of desk:'))
+symbol = input ('type symbol: ')
 
 # create outer and inner range based on input
 
@@ -6,13 +7,20 @@ for_outer = (lenght_of_desk // 2)
 for_inner = for_outer
 if (lenght_of_desk % 2) == 1:
     for_outer += 1
+chess = []
+for i in range (for_outer):
+    chess.append(symbol)
+    chess.append(' ')
+chess = chess [:lenght_of_desk]
+print (chess)
+
 
 # print (for_outer)
 # print (for_inner)
 
 # for i in range(len(lenght_of_desk))
-for i in range(6):
-    print('* ', end="")
-    for j in range(for_inner):
-        print(' *', end="")
-    print('')
+# for i in range(6):
+#     print('* ', end="")
+#     for j in range(for_inner):
+#         print(' *', end="")
+#     print('')
