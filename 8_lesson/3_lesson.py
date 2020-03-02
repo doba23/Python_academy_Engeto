d183bc6601f4b14d1e7d87980ed8dcaf5dbac1d4
Dominik Bauchner <dbauchner@gmail.com>
2020-03-02 20:29:34 +0100

    lesson work

--- 8_lesson/3_lesson.py
+++ 8_lesson/3_lesson.py
@@ -11,12 +11,12 @@ def our_better_print (*args, **kwargs):
 
 our_better_print('Test' ,'ate', 4, 5, key = 'value', key2= 4)
 
-def lsit_divisibles(start, stop, divisor):
-    divisibles= []
-    for num in range (start, stop):
-        if num %
-
-args - {'start':1, 'stop':3, 'divisor':5}
-print
-
-# wrapper
+# def lsit_divisibles(start, stop, divisor):
+#     divisibles= []
+#     for num in range (start, stop):
+#         if num %
+#
+# args - {'start':1, 'stop':3, 'divisor':5}
+# print
+#
+# # wrapper
