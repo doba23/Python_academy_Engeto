180fae499d8d245a5828e90d242c353b6b9b3093
Dominik Bauchner <dbauchner@gmail.com>
2020-03-02 16:15:43 +0100

    homework try

--- 6_lesson/40.py
+++ 6_lesson/40.py
@@ -1,29 +1,29 @@
-# def all (own_list):
-#     boolean = 'False' if own_list != 1 else 'True'
-#
-#     for item in own_list:
-#         boolean = (bool (item))
-#         # print(item, boolean)
-#         if (bool (item)) == 0:
-#             boolean = 'False'
-#             break
-#
-#     return boolean
+def all (own_list):
+    boolean = 'False' if own_list != 1 else 'True'
+
+    for item in own_list:
+        boolean = (bool (item))
+        # print(item, boolean)
+        if (bool (item)) == 0:
+            boolean = 'False'
+            break
+
+    return boolean
 # print (all([]))
 
 def any (my_list):
     boolean_2 = 'False' if bool (my_list) == 0 else 'True'
-    print('Boolean od list' boolean_2)
+    # print('Boolean od list' boolean_2)
     if boolean_2 == 'True':
-        print (boolean_2 == 1, 'second step')
+        boolean_2 = 'False'
+        # print (boolean_2 == 1, 'second step')
         for item_2 in my_list:
-            print('run of for loop')
+            # print('run of for loop')
             if bool(item_2) == 1:
+                boolean_2 = 'True'
                 break
             else:
                 continue
-        # boolean_2 = 'True'  and boolean_2 = 'True') else 'False'
-            print(item_2, boolean_2)
     return boolean_2
 
-print (any([0,0,0,1]))
+# print (any([1,0,1,0,0,1,0]))
