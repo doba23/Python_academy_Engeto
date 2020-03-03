7e3f26f99d839c213cd53ef2f6dbc5434cd99c0e
dell <dbauchner@gmail.com>
2020-03-03 16:58:49 +0100

    uloha z vyucovani

--- 8_lesson/lesson_args.py
+++ 8_lesson/lesson_args.py
@@ -2,11 +2,31 @@ def func(prefix, suffix, *argumenty):
     for arg in argumenty:
         print(prefix + arg + suffix)
 
+
 func('in', 'ly', 'competent', 'formal', 'credib')
+
+
+print('')
+
 
 def name_f(prefix, suffix, *args, **kwargs):
     for arg in args:
         if 'capital' in kwargs and kwargs['capital'] == True:
             arg = arg.upper()
-        print (prefix+arg+suffix)
-name_f('la','!', 'mbada', 'mbda', 'corida', capital=True)
+        print(prefix + arg + suffix)
+
+
+name_f('la', '!', 'mbada', 'mbda', 'corida', capital=True)
+
+
+print('')
+
+
+def name_g(prefix, suffix, *args, capital):
+    for arg in args:
+        if capital:
+            arg = arg.upper()
+        print(prefix + arg + suffix)
+
+
+name_g('la', '!', 'mbada', 'mbda', 'corida', capital=True)
