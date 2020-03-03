a6349804ee2926e04f9701e7a000142047efa54d
Dominik Bauchner <dbauchner@gmail.com>
2020-03-03 19:46:13 +0100

    lesson work

---
+++ 6_lesson/30.py
@@ -0,0 +1,5 @@
+user_string = str(input('Hello, please write your numbers and press enter to confirm:'))
+user_string = user_string.replace(' ', '')
+user = user_string.split(',')
+
+print('List:',user)
