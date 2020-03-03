75c26438c491aaceb6f9d06bf481da6c0233135b
Dominik Bauchner <dbauchner@gmail.com>
2020-03-03 21:13:12 +0100

    home work

---
+++ 7_ lesson/41.py
@@ -0,0 +1,8 @@
+def reverse (my_str):
+    my_str = list (my_str)
+    reversed = []
+    for item in range(len(my_str)):
+        reversed.append(my_str.pop())
+    return reversed
+
+# print (reverse('abcdefgh'))
