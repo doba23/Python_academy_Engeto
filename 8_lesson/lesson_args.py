cf41f97005dde6549195699fdee1922a00f39792
dell <dbauchner@gmail.com>
2020-03-03 16:45:53 +0100

    uloha z vyucovani

--- 8_lesson/lesson_args.py
+++ 8_lesson/lesson_args.py
@@ -3,3 +3,10 @@ def func(prefix, suffix, *argumenty):
         print(prefix + arg + suffix)
 
 func('in', 'ly', 'competent', 'formal', 'credib')
+
+def name_f(prefix, suffix, *args, **kwargs):
+    for arg in args:
+        if 'capital' in kwargs and kwargs['capital'] == True:
+            arg = arg.upper()
+        print (prefix+arg+suffix)
+name_f('la','!', 'mbada', 'mbda', 'corida', capital=True)
