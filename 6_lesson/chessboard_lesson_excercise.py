ad2624cd3d6c2ec52d06a3068246c2d7e38bcb45
dell <dbauchner@gmail.com>
2020-03-03 16:10:51 +0100

    uloha z vyucovani

---
+++ 6_lesson/chessboard_lesson_excercise.py
@@ -0,0 +1,6 @@
+num = int(input ('Write a number:'))
+lst= []
+
+for chess in range (int(num /2)):
+    lst.append ('# ')
+print (str(lst))
---
+++ 8_lesson/lesson_args.py
@@ -0,0 +1,5 @@
+def func(prefix, suffix, *argumenty):
+    for arg in argumenty:
+        print(prefix + arg + suffix)
+
+func('in', 'ly', 'competent', 'formal', 'credib')
