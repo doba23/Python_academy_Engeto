6a17dd57064db1d1d45fb90fb106cea93c23cd9e
Dominik Bauchner <dbauchner@gmail.com>
2020-03-04 22:37:02 +0100

    home work

--- 8_lesson/47.py
+++ 8_lesson/47.py
@@ -1,18 +1,30 @@
 def all_anagrams (args):
 
 
-    for first_in_list in args:
-        first = first_in_list
-        break
-    print (first)
-    for outer_item in args:
-        (print('outer item',outer_item))
-        # print (args[0])
-        if first_in_list == outer_item:
-            return True
-        else:
-            return False
-    # for inner_item in outer_item:
-    #     print (inner_item)
+    # for first_in_list in args:
+    #     first = first_in_list
+    #     break
+    # print (first)
+    # for outer_item in args:
+    #     (print('outer item',outer_item))
+    #     # print (args[0])
+    #     if first_in_list == outer_item:
+    #         return True
+    #     else:
+    #         return False
+    #
+    # print(all_anagrams(['ship', 'pish']))
 
-print(all_anagrams (['ship', 'pish']))
+
+    for outer_item in args:
+        # print(set(outer_item))
+        # print (set(args[0]))
+
+        # set(outer_item).issubset(set(args[0]))
+
+        # print (args[0])
+
+
+
+
+print(all_anagrams (['ship']))
