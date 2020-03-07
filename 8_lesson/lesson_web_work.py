425b38883360d029a16aa29cd25a71e481b8c7ab
Dominik Bauchner <dbauchner@gmail.com>
2020-03-07 15:39:04 +0100

    home work

--- 8_lesson/lesson_web_work.py
+++ 8_lesson/lesson_web_work.py
@@ -7,3 +7,10 @@ def divisible_by_range (start, stop, divisor):
 
 print (divisible_by_range(4,8,3))
 print (divisible_by_range(stop=8,divisor=3,start=4))
+
+def multiply(num, multiplier=2):
+    return num * multiplier
+print (multiply(2))
+
+print (multiply(2,8))
+print (multiply(2, multiplier=8))
