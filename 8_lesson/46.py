65f573f77ecbc7bd8b66982f06c6a0790b57bac4
Dominik Bauchner <dbauchner@gmail.com>
2020-03-08 16:58:44 +0100

    home work

--- 8_lesson/46.py
+++ 8_lesson/46.py
@@ -1,9 +1,32 @@
+def reverse_digit(card_number):
+    number_reversed = ''
+    card_number = str (card_number)
+    while card_number:
+        number_reversed = number_reversed+card_number[-1]
+        card_number = card_number[:-1]
+    return number_reversed
 
+print (reverse_digit(49927398716))
 
 def s1 (number):
     total = 0
     for i in range (0,len(number),2):
         total += int(number[i])
     return total
-print (s1('61789372994'))
 
+print (s1(reverse_digit(49927398716)))
+
+def s2 (number):
+    total = 0
+    final_list = []
+    for i in range (1,len(number),2):
+        # print (number[i])
+        multiply = (int(number [i]))*2
+        if multiply < 10:
+            final_list.append(multiply)
+        else:
+            # print ((multiply[0]),'+',(multiply[1]))
+            final_list.append(multiply[0]+(multiply[1])
+    return final_list
+
+print (s2(reverse_digit(49927398716)))
