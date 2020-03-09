4ad5fe8d97a5e3eb95c968e49010e622025424de
Dominik Bauchner <dbauchner@gmail.com>
2020-03-09 18:41:07 +0100

    home work

---
+++ 9_lesson/lesson_2.py
@@ -0,0 +1,4 @@
+celsius = int(input('What is the current temperature?'))
+fahrenheit = celsius * (9/5) + 32
+name='Said Dominik'
+print('{} degrees Celsius would be {} degrees Fahrenheit.{}'.format(celsius, fahrenheit,name))
