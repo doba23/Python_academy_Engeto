7e848e53477977c44445f038c0c7d9bcbef0e8ff
Dominik Bauchner <dbauchner@gmail.com>
2020-03-09 19:35:14 +0100

    lesson work (at home)

--- 9_lesson/lesson_use_%s.py
+++ 9_lesson/lesson_use_%s.py
@@ -2,3 +2,7 @@ name = input('Whats your name?')
 age = int (input ('How old are you?'))
 
 print ('My name is %s. I am %d year old.' %(name, age))
+
+fomated_string = 'My name\'s %s. I\'am %d yaer old.'
+reference = (name, age)
+print(fomated_string % reference)
