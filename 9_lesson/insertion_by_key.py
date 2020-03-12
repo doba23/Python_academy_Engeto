6a6651e2f217b243b2ec8d2c3ea5395125c728a4
Dominik Bauchner <dbauchner@gmail.com>
2020-03-12 19:03:23 +0100

    lesson work (at home)

--- 9_lesson/insertion_by_key.py
+++ 9_lesson/insertion_by_key.py
@@ -1,7 +1,15 @@
-data= {'name':'Dominik', 'age':'28'}
-print_it = 'My name is %(name)s. I am %(age)s year old.' % data
+person_data= {'name':'Dominik', 'age':'28'}
+
+print_it = 'My name is %(name)s. I am %(age)s year old.' % person_data
 print (print_it)
 
-data= {'name':'Dominik', 'age':'28'}
+
 print_it2 = 'My name\'s {name}. I \'m {age} year old'.format(name ='Dominik', age = 28)
 print(print_it2)
+
+formatted = 'My name is {name}. I am {age} years old'.format(**person_data)
+print (formatted)
+
+data = ('Dominik',29)
+formatted_2 = 'My name\'s {}. I\'m {} years old'.format(*data)
+print (formatted_2)
