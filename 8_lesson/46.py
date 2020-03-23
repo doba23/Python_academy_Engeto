5da92d078c669245542fa41baffaaaeb86d081f2
Dominik Bauchner <dbauchner@gmail.com>
2020-03-23 15:37:13 +0100

    home work

--- 8_lesson/46.py
+++ 8_lesson/46.py
@@ -1,3 +1,5 @@
+# Luhn test in Python
+# reverse numbers
 def reverse_digit(card_number):
     number_reversed = ''
     card_number = str (card_number)
@@ -6,27 +8,72 @@ def reverse_digit(card_number):
         card_number = card_number[:-1]
     return number_reversed
 
-print (reverse_digit(49927398716))
+reverse = reverse_digit(49927398716)
+# print (reverse)
 
-def s1 (number):
+# Sum the odd digits
+def s1_func (number):
     total = 0
     for i in range (0,len(number),2):
         total += int(number[i])
     return total
+s1 = s1_func(reverse)
+# print (s1)
 
-print (s1(reverse_digit(49927398716)))
+# chose even digits
+def even(number):
+    even_lst = []
+    for i in range(1, len(number), 2):
+        even_lst.append(number [i])
+    return even_lst
+even_lst = (even (reverse))
+# print (even_lst)
 
-def s2 (number):
-    total = 0
-    final_list = []
-    for i in range (1,len(number),2):
-        # print (number[i])
-        multiply = (int(number [i]))*2
-        if multiply < 10:
-            final_list.append(multiply)
+# multiply each even digit x 2
+def multiply (number):
+    multiply_lst = []
+    for i in number:
+        multiply_lst.append(int(i)*2)
+    return multiply_lst
+multiply_lst = multiply(even_lst)
+# print(multiply_lst)
+
+# if there more digits, sum the digits of each multiplication
+def sum_digits (number):
+    sum_digits_lst = []
+    for i in number:
+        if int (i) < 10:
+            sum_digits_lst.append(i)
         else:
-            # print ((multiply[0]),'+',(multiply[1]))
-            final_list.append(multiply[0]+(multiply[1])
-    return final_list
+            i = str (i)
+            first_digit = int(i[0])
+            second_digit = int(i[1])
+            sum_digits_lst.append((first_digit)+(second_digit))
+    return sum_digits_lst
 
-print (s2(reverse_digit(49927398716)))
+sum_digits_lst = sum_digits (multiply_lst)
+# print (sum_digits_lst)
+
+# sum each digit
+def s2_func (number):
+    total = 0
+    for i in number:
+        i = int (i)
+        total += i
+    return total
+s2 = s2_func(sum_digits_lst)
+# print (s2)
+
+# sum s1+s2 and save to string
+result = str(s1 + s2)
+
+# test result
+def luhn_test (a):
+    if result[1] == '0':
+        print ('True')
+    else:
+        print ('False')
+
+luhn_test(result)
+
+
