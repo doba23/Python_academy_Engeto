08242526854fbaa5c357c36682ea890f801ad0ec
Dominik Bauchner <dbauchner@gmail.com>
2020-03-24 17:12:52 +0100

    home work

---
+++ 8_lesson/69.py
@@ -0,0 +1,15 @@
+def change_coins (number):
+    numbers_l = [50, 20, 10, 5, 2, 1]
+    dict = {}
+
+    for item in numbers_l:
+        dict[item] = 0
+        while number // item != 0:
+            dict[item] += 1
+            number -= item
+        # deletes empty dict items
+        if dict[item] == 0:
+            del dict [item]
+    return (number, dict)
+
+print (change_coins(109))
