9bf61f06d776c58a7a5b4de47e17f432ad95e1c9
Dominik Bauchner <dbauchner@gmail.com>
2020-03-27 12:15:02 +0100

    lesson

--- 9_lesson/lesson_w_+_my.py
+++ 9_lesson/lesson_w_+_my.py
@@ -1,20 +1,21 @@
 file = open('test.txt', 'w+')
 print(file.write('This is the first line\nThis is the second line\nThis is the third line'))
-print (file.seek(0))
+print(file.seek(0))
 
 # obdoba metidy .read()
 for line in file:
-    print (line, end='')
+    print(line, end='')
 
 print()
-print (file.seek(0))
-print ()
+print(file.seek(0))
+print()
 
 # precteni prvniho radku
-print( file.readline())
-print (file.seek(0))
+print(file.readline())
+print(file.seek(0))
 
 # ulozeni vsech radku do listu
-lines_lst=file.readlines(0)
-print (lines_lst)
+lines_lst = file.readlines()
+print(lines_lst)
 
+file.close()
